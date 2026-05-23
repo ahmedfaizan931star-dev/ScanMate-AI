1598f9848f9a67acbbba6ddb573823a8898316cd
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-05-24 04:55:39 +0500

    Add Kotlin Android plugin to build.gradle.kts

--- Scanmate AI/app/build.gradle.kts
+++ Scanmate AI/app/build.gradle.kts
@@ -2,6 +2,7 @@ import java.util.Locale
 
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.google.devtools.ksp)
 }
